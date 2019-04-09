1da17a988e10c41dbefb2cbce2da7c12f9ee7bf5
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2019-04-09 16:30:59 +0200

    Add Openbox window manager and add JavaFX compatibility libs
    fixes https://github.com/gitpod-io/workspace-images/issues/56

--- full-vnc/start-vnc-session.sh
+++ full-vnc/start-vnc-session.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-if [[ $# < 1 || ! $1 ]]; then
-    echo "Usage: $0 DISPLAY"
-    exit 1
-fi
+# see https://en.wikipedia.org/wiki/Xvfb#Remote_control_over_SSH
 
-DISP=$1
+DISP=${DISPLAY:1}
 
 Xvfb -screen $DISP 1920x1080x16 -ac -pn -noreset &
+
+$WINDOW_MANAGER &
 
 VNC_PORT=$(expr 5900 + $DISP)
 NOVNC_PORT=$(expr 6080 + $DISP)
 
-x11vnc -localhost -shared -display :$DISP -forever -rfbport ${VNC_PORT} &> "/tmp/x11vnc-${DISP}.log" &
+x11vnc -localhost -shared -display :$DISP -forever -rfbport ${VNC_PORT} -bg -o "/tmp/x11vnc-${DISP}.log"
 cd /opt/novnc/utils && ./launch.sh --vnc "localhost:${VNC_PORT}" --listen "${NOVNC_PORT}" &
