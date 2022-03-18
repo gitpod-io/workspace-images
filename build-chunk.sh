69431c6afedcbd61caaee05abac2396b14bf777e
Thomas Schubart <24721048+Furisto@users.noreply.github.com>
2022-03-18 14:40:47 +0100

    Add new build scripts (#769)
    * Update yq
    * Add new scripts for building images
    * Remove combo from build-chunk.sh
    * Update docs for build-chunk
    
    Co-authored-by: Kyle Brennan <kyle@gitpod.io>

--- build-chunk.sh
+++ build-chunk.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
 set -euo pipefail
-trap ctrl_c INT
+trap ctrl_c EXIT
 
-readonly YELLOW=$(tput setaf 3)
-readonly NC=$(tput sgr0)
+# shellcheck source=/dev/null
+source build-common.sh
 
-readonly BACKUP_FILE=".dazzle.yaml.orig"
 readonly TEMP_FILE=".dazzle.yaml.temp"
-readonly ORIGINAL_FILE="dazzle.yaml"
-readonly AVAILABLE_CHUNKS=$(ls chunks/)
 readonly REPO="localhost:5000/dazzle"
 
 function usage() {
 	cat <<EOF
-Usage: ./dazzle-up.sh [OPTION]...
-Example: ./dazzle-up.sh  -c lang-c -c dep-cacert-update -n mychangecombo
+Usage: ./build-chunk.sh [OPTION]...
+Example: ./build-chunk.sh -c lang-c -c dep-cacert-update -c lang-go:1.17.5
 Options for build:
   -h, --help      Display this help
   -c, --chunk     Chunk to build, You can build multiple chunks: -c chunk1 -c chunk2. If no chunks are supplied then build using existing config
-  -n, --name      Combination name, by default a combination name 'default' is created. This flag only works when -c flag is specified
 EOF
 }
 
-function restore_original() {
-	echo "${YELLOW}Restoring backup file ${BACKUP_FILE} to original file ${ORIGINAL_FILE}${NC}"
-	cp ${BACKUP_FILE} ${ORIGINAL_FILE}
-}
-
-function ctrl_c() {
-	echo "${YELLOW}** Trapped CTRL-C${NC}"
-	restore_original
-}
-
-function build_and_combine() {
-	dazzle build ${REPO} -v --chunked-without-hash && dazzle build ${REPO} -v && dazzle combine ${REPO} --all -v
+function build_chunks() {
+	dazzle build ${REPO} -v --chunked-without-hash && dazzle build ${REPO} -v
 }
 
 function extract_variants() {
@@ -54,10 +40,9 @@ function extract_variants() {
 ARGS=$(getopt -o h,:c:n: --long help:,chunk:,name: -- "$@")
 eval set -- "${ARGS}"
 
-[ ! -f ${BACKUP_FILE} ] && echo "${YELLOW}Creating a backup of ${ORIGINAL_FILE} as it does not exist yet...${NC}" && cp ${ORIGINAL_FILE} ${BACKUP_FILE}
+save_original
 
 CHUNKS=""
-COMBINATION="default"
 
 while true; do
 	case "$1" in
@@ -75,17 +60,13 @@ while true; do
 		fi
 		shift 2
 		;;
-	-n | --name)
-		COMBINATION="$2"
-		shift 2
-		;;
 	--)
 		shift
 		break
 		;;
 	*)
 		echo Error: unknown flag "$1"
-		echo "${YELLOW}Run 'dazzle-up.sh --help' for usage.${NC}"
+		echo "${YELLOW}Run 'build-chunk.sh --help' for usage.${NC}"
 		exit 1
 		;;
 	esac
@@ -96,9 +77,9 @@ if [[ -z "${CHUNKS[*]}" ]]; then
 	echo "${YELLOW}No chunks specified, will build using the existing ${ORIGINAL_FILE}${NC}"
 else
 	# else build for supplied arguments
-	[ -f ${ORIGINAL_FILE} ] && echo "${YELLOW}Deleting ${ORIGINAL_FILE}, will produce a new config with supplied arguments${NC}" && rm ${ORIGINAL_FILE}
+	[ -f "${ORIGINAL_FILE}" ] && echo "${YELLOW}Deleting ${ORIGINAL_FILE}, will produce a new config with supplied arguments${NC}" && rm "${ORIGINAL_FILE}"
 
-	for CN in ${AVAILABLE_CHUNKS}; do
+	for CN in $(get_available_chunks); do
 		if [[ ! ${CHUNKS[*]} =~ ${CN} ]]; then
 			dazzle project ignore "${CN}"
 		else
@@ -109,12 +90,10 @@ else
 		fi
 	done
 
-	dazzle project add-combination "${COMBINATION}" "${CHUNKS_TO_COMBINE[@]}"
+	# dazzle project add-combination "${COMBINATION}" "${CHUNKS_TO_COMBINE[@]}"
 fi
 
-build_and_combine
+build_chunks
 
 echo "${YELLOW}Saving dazzle config used to generate build in ${TEMP_FILE}${NC}"
-cp ${ORIGINAL_FILE} ${TEMP_FILE}
-
-restore_original
+cp "${ORIGINAL_FILE}" ${TEMP_FILE}
