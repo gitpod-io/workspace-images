746f4612ba9944146421f57c76bd28818a4f8d93
AXON <axon@gitpod.io>
2022-07-07 18:19:57 +0000

    Update tests

--- chunks/lang-python/avoid_userbase_hook.bash
+++ chunks/lang-python/avoid_userbase_hook.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # When someone tries to install to userbase from a custom dockerfile but `/workspace` doesn't exist there
-if [[ "$*" =~ pip[0-9]?\ install.*--user ]] && test ! -v GITPOD_REPO_ROOT; then {
+if [[ "$*" =~ pip.*install.*--user ]] && test ! -e "$GITPOD_REPO_ROOT"; then {
 	for ((i = $#; i != 0; i--)); do {
 		if test "$(eval "echo \$$i")" == "--user"; then {
 			set -- "${@:1:$i-1}" "${@:$i+1}" # Removes the `--user` argument
