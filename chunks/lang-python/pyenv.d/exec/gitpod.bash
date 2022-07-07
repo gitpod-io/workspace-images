c4485ac9864c38be236af53cb51b64e349bee02b
AXON <axon@gitpod.io>
2022-07-07 17:34:21 +0000

    feat: Mirror amd emulate pyenv-shims for USERBASE instead of TARGET
    Signed-off-by: AXON <axon@gitpod.io>

--- chunks/lang-python/pyenv.d/exec/gitpod.bash
+++ chunks/lang-python/pyenv.d/exec/gitpod.bash
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-export PYTHONPATH="${GP_PYENV_MIRROR}/site/${PYENV_VERSION}" # `pip install` will use this path.
-# Do not set PIP_TARGET when `--user` arg is passed to `pip install` to avoid
-# ERROR: Can not combine '--user' and '--target'.
-if [[ ! "$*" =~ pip[0-9]?\ install.*--user ]]; then {
-	export PIP_TARGET="$PYTHONPATH" PIP_UPGRADE=true
+# export PYTHONPATH="${GP_PYENV_MIRROR}/site/${PYENV_VERSION}" # `pip install` will use this path.
+# # Do not set PIP_TARGET when `--user` arg is passed to `pip install` to avoid
+# ## ERROR: Can not combine '--user' and '--target'.
+# if [[ ! "$*" =~ pip[0-9]?\ install.*--user ]]; then {
+# 	export PIP_TARGET="$PYTHONPATH" PIP_UPGRADE=true
+# }; fi
+
+# Do not set PIP_USER when `--target` arg is passed to `pip install` to avoid
+## ERROR: Can not combine '--user' and '--target'.
+if [[ ! "$*" =~ pip.*install.*--target ]]; then {
+	export PYTHONUSERBASE="$GP_PYENV_MIRROR/user/$PYENV_VERSION" \
+		PIP_USER=true \
+		PIP_NO_WARN_SCRIPT_LOCATION=false
 }; fi
--- chunks/lang-python/pyenv.d/which/gitpod.bash
+++ chunks/lang-python/pyenv.d/which/gitpod.bash
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
-if test ! -x "$PYENV_COMMAND_PATH"; then {
-	for bin in "${GP_PYENV_MIRROR}/site/"*"/bin/$PYENV_COMMAND"; do {
-		PYENV_COMMAND_PATH="$bin"
-		if test -x "$PYENV_COMMAND_PATH"; then {
-			break
-		}; fi
-	}; done
+# if test ! -x "$PYENV_COMMAND_PATH"; then {\
+## Give user variant higher priority
+# shellcheck disable=SC2154
+_check_upath="${GP_PYENV_MIRROR}/user/${versions[0]}/bin/$PYENV_COMMAND"
+if test -x "$_check_upath"; then {
+	# shellcheck disable=SC2034
+	PYENV_COMMAND_PATH="$_check_upath"
 }; fi
+
+# Hijack into `pyenv-prefix` to be called from pyenv-whence <arg>
+# This is necessary for pyenv-which to recognize an installed but
+## unselected python version containing the program.
+function pyenv-prefix {
+	# Add user prefix paths when called by pyenv-prefix
+	if test "${BASH_SOURCE[1]}" == "$PYENV_ROOT/libexec/pyenv-whence"; then {
+		local target="${GP_PYENV_MIRROR}/user/$1"
+		if test -e "$target"; then {
+			echo "$target"
+			return
+		}; fi
+	}; fi
+	builtin command pyenv-prefix "$@"
+} && export -f pyenv-prefix
+# }; fi
