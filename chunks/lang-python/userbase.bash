15ad893609448a835a3f444806985029a2c94e34
AXON <axonasif@gmail.com>
2022-08-03 12:47:41 +0000

    fix: userebase.bash

--- chunks/lang-python/userbase.bash
+++ chunks/lang-python/userbase.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if test -e "$GITPOD_REPO_ROOT"; then {
 
-	# For pyenv-global and pyenv-sh-shell hook
-	if test ! -v PYENV_VERSION; then {
+	# For pyenv-global hook
+	if test ! -v PYENV_VERSION && test "${BASH_SOURCE[1]}" == "$PYENV_ROOT/libexec/pyenv-global"; then {
 		# shellcheck disable=SC2154
 		PYENV_VERSION="${versions[0]}"
 		if [[ ! "$PYENV_VERSION" =~ \. ]]; then {
@@ -24,12 +24,14 @@ if test -e "$GITPOD_REPO_ROOT"; then {
 	mounted_version="$(<"$mounted_version_file")"
 
 	# shellcheck disable=SC2034
-	if ! mountpoint -q "$system_python_userbase" && not_mounted=true || test "$mounted_version" -ne "$pyenv_version"; then {
-		mkdir -p "$system_python_userbase" 2>/dev/null || :
+	if ! mountpoint -q "$system_python_userbase" && not_mounted=true || test "$mounted_version" != "$pyenv_version"; then {
+		mkdir -p "$system_python_userbase" "$versioned_python_userbase_dir" 2>/dev/null
 		if test ! -v not_mounted; then {
-			sudo umount -lf "$system_python_userbase" 2>/dev/null || :
+			while mountpoint -q "$system_python_userbase"; do {
+				sudo umount "$system_python_userbase" || sudo umount -l "$system_python_userbase"
+			}; done
 		}; fi
-		if ! sudo mount --bind "$versioned_python_userbase_dir" "$system_python_userbase" 2>/dev/null; then {
+		if ! sudo mount --bind "$versioned_python_userbase_dir" "$system_python_userbase"; then {
 			rm -rf "$system_python_userbase"
 			ln -s "$versioned_python_userbase_dir" "$system_python_userbase"
 		}; fi
