5602bd6a48a8265f568fdced4848ad2649db4d6d
AXON <axonasif@gmail.com>
2022-08-04 13:21:25 +0000

    Some changes

--- chunks/lang-ruby/ruby_hook.bash
+++ chunks/lang-ruby/ruby_hook.bash
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-# shellcheck source=/dev/null
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
 function gitpod_init() {
 	if test -e "$GITPOD_REPO_ROOT"; then {
 		local rvmrc_string="rvm_gems_path=/workspace/.rvm"
@@ -11,10 +7,11 @@ function gitpod_init() {
 			printf '%s\n' "$rvmrc_string" >>"$rvmrc_file"
 		}; fi
 		export GEM_HOME="/workspace/.rvm"
-	}; else {
-		export GEM_HOME="$HOME/.rvm"
 	}; fi
 
+	local rvm_script="$HOME/.rvm/scripts/rvm"
+	# shellcheck source=/dev/null
+	[[ -s "$rvm_script" ]] && source "$rvm_script"
 	export PATH="$GEM_HOME/bin:$PATH"
 	export GEM_PATH="$GEM_HOME:$GEM_PATH"
 }
