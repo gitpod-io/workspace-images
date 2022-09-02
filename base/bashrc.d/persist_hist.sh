67df02def2be2a700485d526205510db6d1e9c65
AXON <axonasif@gmail.com>
2022-09-02 19:18:56 +0000

    feat: Persist shell(bash,zsh,fish) histories between workspace reboots

---
+++ base/bashrc.d/persist_hist.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+if test -e "$GITPOD_REPO_ROOT" && test ! -v HIST_PERIST_INIT && export HIST_PERIST_INIT=true; then {
+	# Taken from https://github.com/axonasif/dotfiles/blob/34c8663cac2656cdfb0ade40bcf6317c97cc239d/src/config/shell.sh#L7
+	function config::shell::persist_history() {
+		local -r _shell_hist_files=(
+			# Not going to use HISTFILE env var for bash and zsh
+			"$HOME/.bash_history"
+			"$HOME/.zsh_history"
+			"$HOME/.local/share/fish/fish_history"
+		)
+
+		# Use workspace persisted history
+		local _workspace_persist_dir="/workspace/.persist"
+		mkdir -p "$_workspace_persist_dir"
+		local _hist
+		for _hist in "${_shell_hist_files[@]}"; do {
+			mkdir -p "${_hist%/*}"
+			_hist_name="${_hist##*/}"
+			if test ! -e "$_workspace_persist_dir/$_hist_name"; then {
+				printf '' >"$_hist"
+				mv "$_hist" "$_workspace_persist_dir/"
+			}; fi
+			ln -sf "$_workspace_persist_dir/${_hist_name}" "$_hist"
+			unset _hist_name
+		}; done
+	}
+	config::shell::persist_history || :
+}; fi
