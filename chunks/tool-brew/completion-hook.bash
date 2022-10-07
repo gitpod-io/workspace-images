7553ad4a0e63f67d13516a806fff00422575d41b
Shyim <github@shyim.de>
2022-10-07 23:16:36 +0200

    feat: add linuxbrew completion support for bash

---
+++ chunks/tool-brew/completion-hook.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
