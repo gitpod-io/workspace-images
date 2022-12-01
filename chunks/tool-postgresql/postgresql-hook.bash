d41c028d6719932dc44ff70d19fa69614e56d5d9
AXON <axonasif@gmail.com>
2022-12-01 22:05:23 +0600

    Update postgresql-hook.bash

--- chunks/tool-postgresql/postgresql-hook.bash
+++ chunks/tool-postgresql/postgresql-hook.bash
@@ -12,9 +12,13 @@
 			}; fi
 
 			if ! [[ "$(pg_ctl status)" =~ PID ]]; then {
-				pg_start >/dev/null
+				printf 'INFO: %s\n' "Executing command: pg_start"
+				pg_start
 				trap "pg_stop" TERM EXIT
 				exec {sfd}<> <(:)
+				printf 'INFO: %s\n' \
+					"Please create another terminal" \
+					"this one is monitoring postgres server for gracefully shutting down when needed"
 				until read -r -t 3600 -u $sfd; do continue; done
 			}; fi
 
