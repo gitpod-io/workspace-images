08c99d2da49cf9305635697eaa94e5af33325d02
Filip Troníček <filip@gitpod.io>
2023-10-04 07:25:38 +0000

    Go use GH actions version file

--- .autofix/fixers/update-lang-go.js
+++ .autofix/fixers/update-lang-go.js
@@ -1,29 +1,28 @@
 const os = require('os');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 
 exports.register = async (fixers) => {
-  const { stdout, stderr } = await exec('gvm listall');
-  if (stderr) {
-    throw stderr;
-  }
+  const response = await fetch("https://raw.githubusercontent.com/actions/go-versions/main/versions-manifest.json");
+  const data = await response.json();
+
+  const versions = data.filter(version => version.stable).flatMap(version => version.version).reverse();
 
   const patchVersionReplacements = {};
-  for (const line of stdout.split('\n').slice(1,-1)) {
-    const match = line.trim().match(/^go(\d+\.\d+)\.(\d+)$/);
+  for (const version of versions) {
+    const match = version.match(/^(\d+\.\d+)\.(\d+)$/);
     if (!match) {
       continue;
     }
-
     // Only capturing major and minor versions in the pattern
-    const pattern = match[1].replace(/\./g, '\\.') + '\\.[0-9][0-9]*';
-    patchVersionReplacements[pattern] = match[1] + '.' + match[2];
+    const segments = version.split('.');
+    const prefix = segments.slice(0, -1).join('\\.');
+    const pattern = prefix + '\\.[0-9][0-9]*';
+    patchVersionReplacements[pattern] = version;
   }
 
   fixers[0].push({
     id: 'update-lang-go',
     cmd: Object.keys(patchVersionReplacements).map(pattern => {
-        return `sed ${os.type() === 'Darwin' ? '-i "" -E' : '-i -e'} "s/\\(GO_VERSION.*\\)${pattern}/\\1${patchVersionReplacements[pattern]}/g" chunks/lang-go/chunk.yaml`;
+      return `sed ${os.type() === 'Darwin' ? '-i "" -E' : '-i -e'} "s/\\(GO_VERSION.*\\)${pattern}/\\1${patchVersionReplacements[pattern]}/g" chunks/lang-go/chunk.yaml`;
     }).join('\n'),
     description: 'update lang go',
   });
